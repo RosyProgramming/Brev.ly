65dfa503b3e5577f9cb80546854eb673ab0bc068
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-04-17 09:57:20 -0300

    not link exists

--- server/src/infra/http/routes/create-links.ts
+++ server/src/infra/http/routes/create-links.ts
@@ -12,21 +12,27 @@ export const createLinks: FastifyPluginAsyncZod = async server => {
         body: createLinkInput,
         response: {
           201: z
-            .object({ id: z.string() })
+            .object({ idLink: z.string() })
             .describe('Link created successfully'),
           400: z.object({ message: z.string() }).describe('Invalid URL format'),
         },
       },
     },
     async (request, reply) => {
-      const { originalUrl, shortUrl } = request.body
-
-      const link = await createLink({
-        originalUrl,
-        shortUrl,
-      })
-
-      return reply.status(201).send({ id: link.id })
+        try {
+          const { originalUrl, shortUrl } = createLinkInput.parse(request.body)
+  
+          const link = await createLink({ originalUrl, shortUrl })
+  
+          return reply.status(201).send({ idLink: link.id })
+        } catch (err) {
+          if (err instanceof Error && err.message === 'Short URL already exists') {
+            return reply.status(400).send({ message: err.message })
+          }
+  
+          console.error(err) // Útil pra debug local
+          return reply.status(500).send({ message: 'Internal server error' })
+        }
     }
   )
 }
