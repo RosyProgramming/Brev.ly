dccb254f2346583756a8379fbe4d2472e2487ae0
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-04-17 09:23:28 -0300

    fix create link

--- server/src/app/functions/create-link.ts
+++ server/src/app/functions/create-link.ts
@@ -1,13 +1,14 @@
 import { db } from '@/infra/db'
 import { schema } from '@/infra/db/schemas'
+import { eq } from 'drizzle-orm'
 import { uuidv7 } from 'uuidv7'
 import { z } from 'zod'
-import { eq } from 'drizzle-orm'
 
 // Schema de validação com zod
 export const createLinkInput = z.object({
   originalUrl: z.string().url({ message: 'Invalid URL' }),
-  shortUrl: z.string()
+  shortUrl: z
+    .string()
     .min(5, { message: 'Short URL must be at least 5 characters long' })
     .regex(/^[a-zA-Z0-9_-]+$/, { message: 'Invalid short URL format' }),
 })
--- server/src/infra/http/routes/create-links.ts
+++ server/src/infra/http/routes/create-links.ts
@@ -1,33 +1,32 @@
+import { createLink, createLinkInput } from '@/app/functions/create-link'
 import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
-import { schema } from '@/infra/db/schemas'
-import { db } from '@/infra/db'
-import { uuidv7 } from 'uuidv7'
 import { z } from 'zod'
-import { createLinkInput, createLink } from '@/app/functions/create-link'
 
 export const createLinks: FastifyPluginAsyncZod = async server => {
-    server.post(
-        '/link',
-        {
-            schema: {
-                summary: 'Create Link',
-                tags: ['links'],
-                body: createLinkInput,
-                response: {
-                    201: z.object({ id: z.string() }).describe('Link created successfully'),
-                    400: z.object({ message: z.string() }).describe('Invalid URL format'),
-                },
-            },
+  server.post(
+    '/link',
+    {
+      schema: {
+        summary: 'Create Link',
+        tags: ['links'],
+        body: createLinkInput,
+        response: {
+          201: z
+            .object({ id: z.string() })
+            .describe('Link created successfully'),
+          400: z.object({ message: z.string() }).describe('Invalid URL format'),
         },
-        async (request, reply) => {
-            const { originalUrl, shortUrl } = request.body
-      
-            const link = await createLink({
-              originalUrl,
-              shortUrl,
-            })
-      
-            return reply.status(201).send({ id: link.id })
-          }
-    );
+      },
+    },
+    async (request, reply) => {
+      const { originalUrl, shortUrl } = request.body
+
+      const link = await createLink({
+        originalUrl,
+        shortUrl,
+      })
+
+      return reply.status(201).send({ id: link.id })
+    }
+  )
 }
