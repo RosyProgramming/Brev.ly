a652cb12a70b05e1a6e21cc93d5d3bcf1cd78220
Rosana Oliveira <rosanadeoliveira0215@gmail.com>
2025-05-02 23:12:48 -0300

    ajuste em links

--- web/src/components/links.tsx
+++ web/src/components/links.tsx
@@ -3,10 +3,10 @@ import { Button } from "./ui/button";
 
 export function Links() {
     return (
-        <div className="flex flex-row items-center py-[2px] gap-5">
-            <div className="flex flex-col justify-center items-center gap-1 flex-none order-0 flex-grow">
-                <span className="font-semibold text-sm leading-[18px] text-blue-base flex-none order-0 self-stretch grow-0">shortUrl</span>
-                <span className="font-normal text-xs text-gray-200 leading-4 flex-none order-1 self-stretch grow-0">originalUrl</span>
+        <div className="flex flex-row justify-between items-center py-[2px] gap-5 w-full border-t border-gray-200">
+            <div className="cursor-pointer flex flex-col gap-1 flex-none order-0 flex-grow">
+                <span className="font-semibold text-sm leading-[18px] text-blue-base flex-none order-0 self-stretch grow-0 truncate">shortUrl</span>
+                <span className="font-normal text-xs text-gray-200 leading-4 flex-none order-1 self-stretch grow-0 truncate">originalUrl</span>
             </div>
             <span className="w-[61px] h-4 text-[12px] leading-4 font-normal text-right text-gray-500 order-1 flex-none grow-0">
                 30 acessos
